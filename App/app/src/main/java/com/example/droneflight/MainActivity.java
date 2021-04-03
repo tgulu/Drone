f4b0fd4910ae895d9ff581b4a5e275a3ad73ad54
Thabo Gulu <tg17473@essex.ac.uk>
2021-04-03 02:02:50 +0100

    setting frame parameters

--- App/app/src/main/java/com/example/droneflight/MainActivity.java
+++ App/app/src/main/java/com/example/droneflight/MainActivity.java
@@ -12,3 +12,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
